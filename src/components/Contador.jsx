1893faf9b217f27b84828e563451c3b6a95d1035
Chrisglz14 <Christiang14@hotmail.com.ar>
2024-01-17 17:27:08 -0300

    Use memo, memo y useCallBack

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
-import ContadorHook from "./ContadorHook";
+import React, { useCallback, useState } from "react";
 import ContadorHijo from "./ContadorHijo";
 
-
-
 const Contador = () => {
+
+  const [contador, setContador] = useState(0)
+
   const [input, setInput] = useState("");
 
   const handleInput = (e) => setInput(e.target.value);
 
-  const { contador, suma, resta, reiniciar } = ContadorHook();
+  // const suma = () => setContador(contador + 1);
+  const suma = useCallback(() => setContador(contador + 1), [contador]);
+
+  const resta = useCallback(() => setContador(contador - 1), [contador]);
+  const reiniciar = useCallback(() => setContador(0), [contador]);
   return (
     <>
       <h2>Contador: {contador}</h2>
@@ -21,7 +25,12 @@ const Contador = () => {
       <button onClick={reiniciar}>Reiniciar</button>
       <button onClick={resta}>-1</button>
       <input type="text" onChange={handleInput} value={input} />
-      <ContadorHijo />
+      <ContadorHijo
+        contador={contador}
+        suma={suma}
+        resta={resta}
+        reiniciar={reiniciar}
+      />
     </>
   );
 };
--- src/components/ContadorHijo.jsx
+++ src/components/ContadorHijo.jsx
@@ -1,10 +1,38 @@
-import React, { memo } from 'react'
+import React, { memo, useMemo } from "react";
 
-const ContadorHijo = () => {
-    console.log("renderizado de hijo para verificar uso de useMemo")
-    return (
-    <h2>ContadorHijo</h2>
-  )
-}
+const ContadorHijo = ({ contador, suma, resta, reiniciar }) => {
+  // let superNumero = 0;
+
+  // for (let i = 0; i < 1000000000; i++) {
+  //   superNumero++
+
+  // }
+
+  const superNumero = useMemo(() => {
+
+    let numero = 0;
+
+    for (let i = 0; i < 10000000; i++) {
+      numero++;
+    }
+
+    return numero
+
+  }, []);
+
+  console.log("renderizado de hijo para verificar uso de useMemo");
+  return (
+    <>
+      <h2>ContadorHijo</h2>
+      <h3>{contador}</h3>
+      <nav>
+        <button onClick={suma}>+1</button>
+        <button onClick={reiniciar}>Reiniciar</button>
+        <button onClick={resta}>-1</button>
+        <h3>{superNumero}</h3>
+      </nav>
+    </>
+  );
+};
 
 export default memo(ContadorHijo);
